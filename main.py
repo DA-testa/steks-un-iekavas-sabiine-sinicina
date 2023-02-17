81701519f04ff29b443266f50e7bda0361599cd3
sabiine-sinicina <124919082+sabiine-sinicina@users.noreply.github.com>
2023-02-17 11:09:39 +0000

    tests4

--- main.py
+++ main.py
@@ -13,18 +13,16 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket
             opening_brackets_stack.append(Bracket(next, i+1))
 
         if next in ")]}":
-            if opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
     if opening_brackets_stack:
                 return opening_brackets_stack[0].position
     else:
             return "Success"
-            # Process closing bracket
             
 
 def main():

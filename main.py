f407e61c15cb4bafc9c29d904de76e9275dc3dd6
sabiine-sinicina <124919082+sabiine-sinicina@users.noreply.github.com>
2023-02-17 11:03:09 +0000

    test3

--- main.py
+++ main.py
@@ -27,12 +27,22 @@ def find_mismatch(text):
             # Process closing bracket
             
 
-
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    print(mismatch)
-    # Printing answer
+    input_type= input()
+
+    if "F" in input_type:
+        name = input()
+        with open (name) as f:
+            text = f.read()
+            mismatch = find_mismatch(text)
+    elif "I" in input_type:
+        text = input()
+        mismatch = find_mismatch(text)
+        if mismatch == "Success":
+            print("Success")
+        else:
+            print (mismatch)
+    
 
 
 if __name__ == "__main__":
